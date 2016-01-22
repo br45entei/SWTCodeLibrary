dd90342bff11a8740aeb4e75425ec49967ffae08
Brian Entei <br45entei@gmail.com>
2016-01-22 17:14:58 -0600

    Moved byte array classes from JavaWebServer
    Title says all

--- src/com/gmail/br45entei/data/DisposableByteArrayOutputStream.java
+++ src/com/gmail/br45entei/data/DisposableByteArrayOutputStream.java
@@ -5,11 +5,29 @@ import java.io.ByteArrayOutputStream;
 /** @author Brian_Entei */
 public class DisposableByteArrayOutputStream extends ByteArrayOutputStream {
 	
-	/** Wipes this ByteArrayOutputStream's byte[] array and resets the counter */
+	/** Retrieves this ByteArrayOutputStream's byte[] array, then calls
+	 * {@link #dispose()} and returns the retrieved bytes.
+	 * 
+	 * @return The bytes. */
+	public final byte[] getBytesAndDispose() {
+		byte[] buf = this.buf;
+		this.dispose();
+		return buf;
+	}
+	
+	/** Wipes this ByteArrayOutputStream's byte[] array and resets the counter. */
 	public final synchronized void dispose() {
 		this.buf = new byte[0];
 		this.count = 0;
 		System.gc();
+	}
+	
+	/** Calls {@link #dispose()}.
+	 * 
+	 * @see java.io.ByteArrayOutputStream#close() */
+	@Override
+	public final void close() {
+		this.dispose();
 	}
 	
 }
